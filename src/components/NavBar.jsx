39d3585c7d2b6a569cab6214d2fc1b3e052b6449
DarkR4ger <saravanangowtham15@gmail.com>
2024-03-28 11:03:49 +0530

    project initialize

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+'use client'
+import styles from './Navbar.module.css'
+
+const NavBar = () => {
+	return(
+		<nav className={styles.nav}>
+			<div className={styles.label}>
+			</div>
+			<div className={styles.logo}>
+			</div>
+			<div className={styles.list}>
+			</div>
+		</nav>
+	)
+}
+
+export default NavBar;
